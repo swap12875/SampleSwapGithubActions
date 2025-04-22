6aeef91da4fa1cff2f645451e267a4c118826e5e
swap12875 <67191980+swap12875@users.noreply.github.com>
2025-04-22 07:23:58 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
 const app = require('./index');
+let server;
 
 describe('API Tests', () => {
+  beforeAll(() => {
+    server = app.listen(3000);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
   test('GET / should return hello message', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
